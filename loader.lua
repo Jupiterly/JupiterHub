c41ffe6d3cb6f2d9668606ea9706e52d58c67887
Jupiterly <90882309+Jupiterly@users.noreply.github.com>
2022-10-06 09:18:43 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,6 +3,4 @@ if game.PlaceId == 286090429 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Jupiterly/JupiterHub/main/arsenal.lua"))()
 elseif game.PlaceId == 621129760 then --//kat
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Jupiterly/JupiterHub/main/kat.lua"))()
-else --// univeral
-    loadstring(game:HttpGet"https://raw.githubusercontent.com/Jupiterly/JupiterHub/main/universal.lua")()
 end
