10fd1892b3101a6af660a56305d2ae8ccaea8cfa
Jupiterly <90882309+Jupiterly@users.noreply.github.com>
2022-10-06 09:20:37 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,4 +3,6 @@ if game.PlaceId == 286090429 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Jupiterly/JupiterHub/main/arsenal.lua"))()
 elseif game.PlaceId == 621129760 then --//kat
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Jupiterly/JupiterHub/main/kat.lua"))()
+else 
+    game.Players.LocalPlayer:Kick("Unsupported Game")
 end
